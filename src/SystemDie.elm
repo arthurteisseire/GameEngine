4eec0f997fd3b4cc5bf2297ce635621f81cd68e6
Arthur Teisseire <arthurteisseire33@gmail.com>
2023-01-29 20:16:52 +0100

    Remove all components in tables when removing an entity

--- src/SystemDie.elm
+++ src/SystemDie.elm
@@ -8,12 +8,43 @@ import World exposing (World)
 
 updateEntity : EntityId -> World -> World
 updateEntity entityId world =
-    { world
-        | entities =
-            case Table.get entityId world.lifeComponents of
-                Just life ->
-                    EntitySet.removeIf (life.healPoints <= 0) entityId world.entities
+    case Table.get entityId world.lifeComponents of
+        Just life ->
+            if life.healPoints <= 0 then
+                removeEntity entityId world
 
-                Nothing ->
-                    world.entities
+            else
+                world
+
+        Nothing ->
+            world
+
+
+removeEntity : EntityId -> World -> World
+removeEntity entityId world =
+    { entities = EntitySet.remove entityId world.entities
+    , keyboardInputComponents = Table.remove entityId world.keyboardInputComponents
+    , positionComponents = Table.remove entityId world.positionComponents
+    , velocityComponents = Table.remove entityId world.velocityComponents
+    , lifeComponents = Table.remove entityId world.lifeComponents
+    , visualComponents = Table.remove entityId world.visualComponents
+    , attackComponents = Table.remove entityId world.attackComponents
+    , damageComponents = Table.remove entityId world.damageComponents
+    , animationComponents = Table.remove entityId world.animationComponents
+    , turnComponents = Table.remove entityId world.turnComponents
+    , terrainComponents = Table.remove entityId world.terrainComponents
+    , aiComponents = Table.remove entityId world.aiComponents
+    , playerComponents = Table.remove entityId world.playerComponents
+    , entityIdDebug =
+        case world.entityIdDebug of
+            Just id ->
+                if id == entityId then
+                    Nothing
+
+                else
+                    world.entityIdDebug
+
+            Nothing ->
+                Nothing
+    , isPause = world.isPause
     }
